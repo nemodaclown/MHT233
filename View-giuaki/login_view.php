c791849b2183b0359a3daac6316ef96af5e6d2c6
Hoàng Minh Thắng <124120060+nemodaclown@users.noreply.github.com>
2024-05-10 18:11:48 +0700

    Create login_view.php

---
+++ View-giuaki/login_view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <h2>Đăng nhập</h2>
+    <form action="login.php" method="post">
+        <label for="username">Tên đăng nhập:</label><br>
+        <input type="text" id="username" name="usernameForm"><br>
+        <label for="password">Mật khẩu:</label><br>
+        <input type="password" id="password" name="passwordForm"><br><br>
+        <input type="submit" value="Đăng nhập">
+    </form>
+</body>
+</html>
